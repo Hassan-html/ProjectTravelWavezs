72973fa7253a67772989252f642f33082001825f
HSN-CODES <logics.nook@gmail.com>
2024-05-20 01:47:34 +0500

    "this"

--- app/api/models/ticketModel.js
+++
@@ -1,6 +0,0 @@
-import { Schema } from "mongoose";
-import mongoose from "mongoose";
-const TicketSchema = new Schema({}, { strict: false });
-
-export const TicketModel =
-  mongoose.models.tickets || mongoose.model("tickets", TicketSchema);
